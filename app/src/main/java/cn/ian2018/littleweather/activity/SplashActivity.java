ed54b16dbadfc493de3adb2b8aacdffb0ddc1d1c
Mr.Chen <ian2018cs@gmail.com>
2017-02-26 21:20:32 +0800

    添加广告，1.0版完成

---
+++ app/src/main/java/cn/ian2018/littleweather/activity/SplashActivity.java
@@ -0,0 +1,167 @@
+package cn.ian2018.littleweather.activity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.qq.e.ads.splash.SplashAD;
+import com.qq.e.ads.splash.SplashADListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.ian2018.littleweather.R;
+import cn.ian2018.littleweather.util.Constant;
+import cn.ian2018.littleweather.util.ToastUtil;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private RelativeLayout container;
+    private ImageView splashImage;
+    private boolean canJump;    // 是否跳过广告
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 让状态栏透明
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        setContentView(R.layout.activity_splash);
+
+        // 初始化控件
+        initUI();
+
+        // 检查运行时权限
+        checkPermission();
+    }
+
+    /**
+     * 检查运行时权限
+     */
+    private void checkPermission() {
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (!permissionList.isEmpty()) {
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(this, permissions, 1);
+        } else {
+            // 请求开屏广告
+            requestAd();
+        }
+    }
+
+    /**
+     * 请求开屏广告
+     */
+    private void requestAd() {
+        new SplashAD(this, container, Constant.APP_ID, Constant.AD_ID, new SplashADListener() {
+            @Override
+            public void onADDismissed() {
+                // 广告显示完毕 进入主界面
+                next();
+            }
+
+            @Override
+            public void onNoAD(int i) {
+                // 广告加载失败 进入主界面
+                next();
+            }
+
+            @Override
+            public void onADPresent() {
+                // 广告加载成功后 需要隐藏预设图片
+                splashImage.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onADClicked() {
+                // 广告被点击
+            }
+
+            @Override
+            public void onADTick(long l) {
+                // 倒计时回调，返回广告还将被展示的剩余时间。
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0) {
+                    for (int grantResult : grantResults) {
+                        if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                            ToastUtil.show("请您同意所有权限再使用小天气");
+                            finish();
+                            return;
+                        }
+                    }
+                    requestAd();
+                } else {
+                    ToastUtil.show("小天气出了个小错误");
+                    finish();
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        canJump = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (canJump) {
+            next();
+        }
+        canJump = true;
+    }
+
+
+    /**
+     * 进入MainActivity
+     */
+    private void next() {
+        if (canJump) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else {
+            canJump = true;
+        }
+    }
+
+    private void initUI() {
+        container = (RelativeLayout) findViewById(R.id.splash_container);
+        splashImage = (ImageView) findViewById(R.id.splash_image);
+    }
+}
--- app/src/main/java/cn/ian2018/littleweather/util/Constant.java
+++ app/src/main/java/cn/ian2018/littleweather/util/Constant.java
@@ -26,4 +26,14 @@ public class Constant {
      * 和风天气api个人认证key
      */
     public static final String WEATHER_KEY = "a889717fe3f044359c6c736d1c5b14df";
+
+    /**
+     * 腾讯广告联盟应用id
+     */
+    public static final String APP_ID = "1105585573";
+
+    /**
+     * 腾讯广告联盟广告位id
+     */
+    public static final String AD_ID = "4010212448179536";
 }
